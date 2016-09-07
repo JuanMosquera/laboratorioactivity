23efb4c0ee28450fb9b78f8a1dda3e50681a0b56
JuanMosquera <jm1128un@gmail.com>
2016-09-07 03:39:34 -0500

    second commit

---
+++ Lab2Activities/app/src/main/java/gr3/compumovil/udea/edu/co/lab2activities/Lugar2.java
@@ -0,0 +1,29 @@
+package gr3.compumovil.udea.edu.co.lab2activities;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Lugar2 extends Fragment {
+
+
+    public Lugar2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lugar2, container, false);
+    }
+
+}
